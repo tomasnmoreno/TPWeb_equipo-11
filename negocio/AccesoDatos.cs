e30efa84fc544d7d66c923da48ba2244f3a64f1f
tomasnmoreno <tomasnehuenmoreno@gmail.com>
2024-05-11 00:39:57 -0300

    Primer subida

---
+++ negocio/AccesoDatos.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace negocio
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conn;
+        private SqlCommand cmd;
+        private SqlDataReader reader;
+        public SqlDataReader Reader
+        {
+            get { return reader; }
+        }
+        
+        public AccesoDatos()
+        {
+            conn = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true");
+            cmd = new SqlCommand();
+        }
+
+        public void setQuery(string query)
+        {
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = query;
+        }
+
+        public void leer()
+        {
+            cmd.Connection = conn;
+
+            try
+            {
+                conn.Open();
+                reader = cmd.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+
+        public void escribir()
+        {
+            cmd.Connection = conn;
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            if(reader != null)
+                reader.Close();
+            conn.Close();   
+        }
+
+        public void setearParametro(string nombre, object valor)
+        {
+            cmd.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public void ejecutarAccion()
+        {
+            cmd.Connection = conn;
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+    }
+
+}
