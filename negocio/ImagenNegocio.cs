d662256d12605a47820557493aabbf2af815efc0
tomasnmoreno <135773563+tomasnmoreno@users.noreply.github.com>
2024-05-11 15:46:42 -0300

    Add files via upload

---
+++ negocio/ImagenNegocio.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.ComponentModel.Design;
+
+
+namespace negocio
+{
+    public class ImagenNegocio
+    {
+        public List<Imagen> listar(Articulo seleccionado)
+        {
+            List<Imagen> listaImagenes = new List<Imagen>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setQuery("SELECT Id, IdArticulo, ImagenUrl from IMAGENES where @IdArt = IdArticulo;");
+                datos.setearParametro("@IdArt", seleccionado.id);
+                datos.leer();
+
+                while (datos.Reader.Read())
+                {
+                    Imagen aux = new Imagen();
+                    aux.id = (int)datos.Reader["Id"];
+                    aux.idArticulo = (int)datos.Reader["IdArticulo"];
+                    aux.url = (string)datos.Reader["ImagenUrl"];
+
+                    listaImagenes.Add(aux);
+                }
+
+                return listaImagenes;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
