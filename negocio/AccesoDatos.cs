86f212f7c5a33316764abdc35762af6be8e8f22f
unknown <alexisflorres768@gmail.com>
2024-05-13 18:13:18 -0300

    footer con navegacion a pagina carrito y home

--- negocio/AccesoDatos.cs
+++ negocio/AccesoDatos.cs
@@ -20,7 +20,7 @@ namespace negocio
         
         public AccesoDatos()
         {
-            conn = new SqlConnection("server=.\\DEVELOP; database=CATALOGO_P3_DB; integrated security=true");
+            conn = new SqlConnection("server=.\\sqlexpress; database=CATALOGO_P3_DB; integrated security=true");
             cmd = new SqlCommand();
         }
 
